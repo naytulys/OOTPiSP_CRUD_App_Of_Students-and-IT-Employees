acc3f2e01a185a0c3217abb810216f525f0eee9a
Stas Bybikov <bybikovstanislav@gmail.com>
2023-03-16 10:48:36 +0300

    Implement ClassDescription. It Described classes and objects of this classes

--- src/main/java/com/phones/Main.java
+++ src/main/java/com/phones/Main.java
@@ -1,9 +1,32 @@
 package com.phones;
 
+import com.phones.phones.Transmitter;
+import com.phones.phones.mobilePhones.CellPhone;
+import com.phones.phones.mobilePhones.SmartPhone;
+import com.phones.phones.stationaryPhones.RadioPhone;
+import com.phones.phones.stationaryPhones.WiredPhone;
+import com.phones.utils.ClassDescription;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static ObservableList<ClassDescription> classList;
 
+    public static ObservableList<ClassDescription> getClassList() {
+        return classList;
+    }
+
+    public static void main(String[] args) {
+        classList = FXCollections.observableArrayList();
+        classList.addAll(
+                new ClassDescription(SmartPhone.class),
+                new ClassDescription(CellPhone.class),
+                new ClassDescription(RadioPhone.class),
+                new ClassDescription(WiredPhone.class),
+                new ClassDescription(Transmitter.class)
+        );
+        // run mainWindow with args
     }
 
 }
