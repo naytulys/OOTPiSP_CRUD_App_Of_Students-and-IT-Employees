c33a2e53b69e97f3cd3a07b82110849ba7335237
Stas Bybikov <bybikovstanislav@gmail.com>
2023-03-16 11:31:00 +0300

    Add forgotten LocalizedName foк one of classes

--- src/main/java/com/phones/phones/stationaryPhones/RadioPhone.java
+++ src/main/java/com/phones/phones/stationaryPhones/RadioPhone.java
@@ -1,11 +1,14 @@
 package com.phones.phones.stationaryPhones;
 
+import com.phones.annotations.LocalizedName;
 import com.phones.phones.StationaryPhone;
 
+@LocalizedName("Radio phone")
 public class RadioPhone extends StationaryPhone {
     private String waveRange;
     private String signalRange;
 
+    @LocalizedName("Wave range")
     public String getWaveRange() {
         return waveRange;
     }
@@ -14,6 +17,7 @@ public class RadioPhone extends StationaryPhone {
         this.waveRange = waveRange;
     }
 
+    @LocalizedName("Signal range")
     public String getSignalRange() {
         return signalRange;
     }
