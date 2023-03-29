f49c2f567b88998b3dbaed874cb4434a2a6c1b7f
Stas Bybikov <bybikovstanislav@gmail.com>
2023-03-29 11:40:14 +0300

    Implements Binary/Text/XML Serializers templates

---
+++ src/main/java/com/serializers/XMLSerializer.java
@@ -0,0 +1,17 @@
+package com.serializers;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+public class XMLSerializer implements Serializer{
+    @Override
+    public void serialize(ArrayList<Object> objectListToWrite, OutputStream outputStream) {
+
+    }
+
+    @Override
+    public ArrayList<Object> deserialize(InputStream inputStream) {
+        return null;
+    }
+}
