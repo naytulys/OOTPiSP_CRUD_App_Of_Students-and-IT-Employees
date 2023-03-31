aa1155cd1e4698ba2296fb52f7fa4030e313a3eb
Stas Bybikov <bybikovstanislav@gmail.com>
2023-03-31 06:47:27 +0300

    Implement XMLSerializer class.

--- src/main/java/com/serializers/XMLSerializer.java
+++ src/main/java/com/serializers/XMLSerializer.java
@@ -2,6 +2,8 @@ package com.serializers;
 
 import com.annotations.LocalizedName;
 
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
@@ -10,11 +12,25 @@ import java.util.ArrayList;
 public class XMLSerializer implements Serializer{
     @Override
     public void serialize(ArrayList<Object> objectListToWrite, OutputStream outputStream) {
-
+        try {
+            XMLEncoder xmlEncoder = new XMLEncoder(outputStream);
+            xmlEncoder.writeObject(objectListToWrite);
+            xmlEncoder.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public ArrayList<Object> deserialize(InputStream inputStream) {
-        return null;
+        Object deserializedObject = null;
+        try {
+            XMLDecoder xmlDecoder = new XMLDecoder(inputStream);
+            deserializedObject = xmlDecoder.readObject();
+            xmlDecoder.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return (ArrayList<Object>)deserializedObject;
     }
 }
