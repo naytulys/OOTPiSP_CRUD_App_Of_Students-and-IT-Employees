f42b9240b8cdfd3f2789853cf0d58ddbac97a01e
Stas Bybikov <bybikovstanislav@gmail.com>
2023-04-02 13:48:05 +0300

    Implement template for deserialize method in TextSerializer class.

--- src/main/java/com/serializers/TextSerializer.java
+++ src/main/java/com/serializers/TextSerializer.java
@@ -7,11 +7,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @LocalizedName("Text Serializer")
 public class TextSerializer implements Serializer{
@@ -48,6 +46,31 @@ public class TextSerializer implements Serializer{
 
     @Override
     public ArrayList<Object> deserialize(Stage parentStage, InputStream inputStream) {
-        return null;
+        ArrayList<Object> resultObjectList = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        try {
+            Object currentObject = null;
+            HashMap<String, String> fields = new HashMap<>();
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("[")) {
+                    String className = line.substring(1, line.length() - 1);
+                    currentObject = Class.forName(className).newInstance();
+                } else if (line.contains("=")) {
+                    String fieldName = line.substring(0, line.indexOf("="));
+                    String fieldValue = line.substring(line.indexOf("=") + 1);
+                    fields.put(fieldName, fieldValue);
+                } else {
+                    /* set fields to current object */
+                    resultObjectList.add(currentObject);
+                    currentObject = null;
+                    fields = new HashMap<>();
+                }
+            }
+            /* restore dependencies */
+        } catch (IOException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return resultObjectList;
     }
 }
